d9fe81846819280c9a1ab0846dac9cf3257564d5
code0em <146350167+Code0Em@users.noreply.github.com>
2024-02-10 08:05:47 +0000

    added user prompts

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
+// **TASKS**
 
+// *TASK 1: Create userPrompts.js and within this create arrays to hold the different prompts for the user to answer (each relating to different role) (answers will then be used to used to generate contents of html).
+// *TASK 2: Export/ import userPrompts.js to index.js (i.e. main js file).
+
+// **IMPORTS**
+// Imports Node's path module
+const path = require("path");
+// Imports Node's file system module
+const fs = require("fs");
+// Imports nmp Inquirer module
+const inquirer = require("inquirer");
+// Task 2: Imports from userPrompts.js (for user prompts)
+const userPrompts = require("./src/userPrompts");
+// Imports from manager.js (for manager class)
+const Manager = require("./lib/manager.js");
+// Imports from engineer.js (for engineer class)
+const Engineer = require("./lib/engineer.js");
+// Imports from intern.js (for intern class)
+const Intern = require("./lib/intern.js");
+// Imports from pageTemplate.js (for html file)
+const render = require("./src/pageTemplate.js");
+
+// **GLOBAL VARIABLES**
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./src/page-template.js");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
