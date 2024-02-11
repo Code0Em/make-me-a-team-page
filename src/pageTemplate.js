6645d9a6e147a5510937ce72a836013a60862ca2
code0em <146350167+Code0Em@users.noreply.github.com>
2024-02-11 12:02:29 +0000

    updated html template

--- src/pageTemplate.js
+++ src/pageTemplate.js
@@ -1,13 +1,15 @@
-// creates the team
+// Task 12 (changes to starter code's html templates): Updated Bootstrap link to newest version. Removed link to own CSS file (redundant). Added inline CSS. Replaced jumbotron with a simple header. Renamed title and h1. Added spacing between cards. Added a simple footer. Added Google font.
+
+// Creates the team.
 const generateTeam = team => {
 
-    // creates the manager html
+    // Creates the manager html.
     const generateManager = manager => {
         return `
         <div class="card employee-card">
         <div class="card-header">
             <h2 class="card-title">${manager.getName()}</h2>
-            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
+            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i> ${manager.getRole()}</h3>
         </div>
         <div class="card-body">
             <ul class="list-group">
@@ -20,13 +22,13 @@ const generateTeam = team => {
         `;
     };
 
-    // creates the html for engineers
+    // Creates the html for engineers.
     const generateEngineer = engineer => {
         return `
         <div class="card employee-card">
     <div class="card-header">
         <h2 class="card-title">${engineer.getName()}</h2>
-        <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
+        <h3 class="card-title"><i class="fas fa-glasses mr-2"></i> ${engineer.getRole()}</h3>
     </div>
     <div class="card-body">
         <ul class="list-group">
@@ -39,19 +41,19 @@ const generateTeam = team => {
         `;
     };
 
-    // creates the html for interns
+    // Creates the html for interns.
     const generateIntern = intern => {
         return `
         <div class="card employee-card">
     <div class="card-header">
         <h2 class="card-title">${intern.getName()}</h2>
-        <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
+        <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i> ${intern.getRole()}</h3>
     </div>
     <div class="card-body">
         <ul class="list-group">
             <li class="list-group-item">ID: ${intern.getId()}</li>
             <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
-            <li class="list-group-item">School: ${intern.getSchool()}</li>
+            <li class="list-group-item">University: ${intern.getSchool()}</li>
         </ul>
     </div>
 </div>
@@ -60,6 +62,7 @@ const generateTeam = team => {
 
     const html = [];
 
+    // 
     html.push(team
         .filter(employee => employee.getRole() === "Manager")
         .map(manager => generateManager(manager))
@@ -79,7 +82,7 @@ const generateTeam = team => {
 
 }
 
-// exports function to generate entire page
+// Exports function to generate entire page.
 module.exports = team => {
 
     return `
@@ -87,32 +90,49 @@ module.exports = team => {
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>My Team</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-    <script src="https://kit.fontawesome.com/c502137733.js"></script>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <title>Software Engineering Team</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+  <script src="https://kit.fontawesome.com/c502137733.js"></script>
+  <style>
+    body {
+      font-family: 'Roboto', sans-serif;
+    }
+
+    header {
+      background-image: url("../images/cyber-technology-background.jpg");
+      background-position: cover;
+    }
+  </style>
 </head>
 
 <body>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-12 jumbotron mb-3 team-heading">
-                <h1 class="text-center">My Team</h1>
-            </div>
-        </div>
+  <header class="container-fluid text-center p-3">
+    <h1>Our Software Engineering Team</h1>
+  </header>
+  <div class="container">
+    <div class="row">
+      <div class="team-area col-12 d-flex justify-content-center gap-3 mt-3">
+        ${generateTeam(team)}
+      </div>
     </div>
-    <div class="container">
-        <div class="row">
-            <div class="team-area col-12 d-flex justify-content-center">
-                ${generateTeam(team)}
-            </div>
-        </div>
+  </div>
+  <footer class="container py-3 my-4 border-top">
+    <div class="row text-center">
+      <a class="col-4 text-body-secondary" href="https://fonts.google.com/specimen/Roboto?query=roboto">Font by Google
+        Font</a>
+      <p class="col-4 text-body-secondary">© 2024 Code Em</p>
+      <a class="col-4 text-body-secondary" href="http://www.freepik.com">Header Image by Freepik</a>
     </div>
+  </footer>
 </body>
+
 </html>
     `;
 };
